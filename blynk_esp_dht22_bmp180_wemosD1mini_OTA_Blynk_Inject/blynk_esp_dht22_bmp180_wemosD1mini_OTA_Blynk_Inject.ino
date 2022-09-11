d74e1051dd4db847b9c7531a8de5ee67c732207a
Grzegorz <dag2ww@gmail.com>
2022-09-11 12:21:03 +0200

    esp now

--- blynk_esp_dht22_bmp180_wemosD1mini_OTA_Blynk_Inject/blynk_esp_dht22_bmp180_wemosD1mini_OTA_Blynk_Inject.ino
+++ blynk_esp_dht22_bmp180_wemosD1mini_OTA_Blynk_Inject/blynk_esp_dht22_bmp180_wemosD1mini_OTA_Blynk_Inject.ino
@@ -1,6 +1,6 @@
 #define BLYNK_TEMPLATE_ID "TMPLyiiIu_zF"
 #define BLYNK_DEVICE_NAME "Klimat Info"
-#define BLYNK_FIRMWARE_VERSION        "0.1.7"
+#define BLYNK_FIRMWARE_VERSION        "0.2.5"
 #define BLYNK_PRINT Serial
 //#define BLYNK_DEBUG
 #define APP_DEBUG
@@ -13,6 +13,57 @@
 #define USE_WEMOS_D1_MINI
 
 #include "BlynkEdgent.h"
+
+//######### ESP NOW COMMS ############
+//#include<ESP8266WiFi.h>
+#include<espnow.h>
+
+#define MY_ROLE         ESP_NOW_ROLE_COMBO              // set the role of this device: CONTROLLER, SLAVE, COMBO
+#define RECEIVER_ROLE   ESP_NOW_ROLE_COMBO              // set the role of the receiver
+#define WIFI_CHANNEL    11
+
+#define MY_NAME         "GATE_NODE" //BC:DD:C2:24:BB:47
+
+uint8_t receiverAddress[] = {0xBC, 0xDD, 0xC2, 0x24, 0xBB, 0x47};   // SENSOR MAC
+//broadcast
+//uint8_t receiverAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};   // SENSOR MAC
+
+struct __attribute__((packed)) dataPacket {
+  int sensor1;
+  int sensor2;
+  float sensor3;
+};
+
+void transmissionComplete(uint8_t *receiver_mac, uint8_t transmissionStatus) {
+  if(transmissionStatus == 0) {
+    Serial.println("ESP-NOW Data sent successfully");
+  } else {
+    Serial.print("ESP-NOW Error code: ");
+    Serial.println(transmissionStatus);
+  }
+}
+
+void dataReceived(uint8_t *senderMac, uint8_t *data, uint8_t dataLength) {
+  char macStr[18];
+  dataPacket packet;  
+
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x", senderMac[0], senderMac[1], senderMac[2], senderMac[3], senderMac[4], senderMac[5]);
+
+  Serial.println();
+  Serial.print("ESP-NOW Received data from: ");
+  Serial.println(macStr);
+  
+  memcpy(&packet, data, sizeof(packet));
+  
+  Serial.print("sensor1: ");
+  Serial.println(packet.sensor1);
+  Serial.print("sensor2: ");
+  Serial.println(packet.sensor2);
+  Serial.print("sensor3: ");
+  Serial.println(packet.sensor3);
+}
+
+//######### ESP NOW COMMS END ############
 
 //for LED status
 //#include <Ticker.h>
@@ -56,6 +107,7 @@ Adafruit_BMP085 bmp;
 
 boolean bmpPresent = false;
 boolean noDhtReported = false;
+boolean macAddrReported = false;
 int dhtReadErrorCount = 0;
 boolean pracaCWUSwitch = false;
 boolean pracaCWUFeedback = false;
@@ -72,6 +124,15 @@ void tickLed()
   //toggle state
   int state = digitalRead(BUILTIN_LED);  // get the current state of GPIO1 pin
   digitalWrite(BUILTIN_LED, !state);     // set pin to the opposite state
+
+  dataPacket packet;
+  
+  packet.sensor1 = 789;
+  packet.sensor2 = 1011;
+  packet.sensor3 = 6.28;
+
+  esp_now_send(receiverAddress, (uint8_t *) &packet, sizeof(packet));
+  
 }
 
 
@@ -82,11 +143,15 @@ void setup() {
   Serial.println();
   Serial.println("Starting...");    //read configuration from FS json
   
+  //WiFi.mode(WIFI_STA);
+  
   BlynkEdgent.begin();
 
   Serial.println("local ip");
   Serial.println(WiFi.localIP());
-
+  Serial.println("MAC ADDRESS:");
+  Serial.println(WiFi.macAddress());
+  Blynk.logEvent("started_info", String("MAC:")+WiFi.macAddress());
   dht.begin();
 
   //SDA, SCL
@@ -100,6 +165,20 @@ void setup() {
   }
 
   blynkTimer.setInterval(2500L, blynkPush);
+
+  timer.setInterval(1000L, tickLed);
+
+  if(esp_now_init() != 0) {
+    Serial.println("ESP-NOW initialization failed");
+    return;
+  }
+
+  esp_now_set_self_role(MY_ROLE);   
+  esp_now_register_send_cb(transmissionComplete);         // this function will get called once all data is sent
+  esp_now_register_recv_cb(dataReceived);               // this function will get called whenever we receive data
+  esp_now_add_peer(receiverAddress, RECEIVER_ROLE, WIFI_CHANNEL, NULL, 0);
+
+  Serial.println("ESP-NOW Initialized.");
 
 }
 //// This function will run every time Blynk connection is established
@@ -123,12 +202,18 @@ void blynkPush()
   // Read temperature as Celsius
   float t = dht.readTemperature();
 
+  if(!macAddrReported){
+    Blynk.logEvent("started_info");
+    Blynk.logEvent("started_info", String("MAC:")+WiFi.macAddress());
+    macAddrReported = true;
+  }
+  
   //t = ((int) (t * 10) / 10.0);
   //h = ((int) (h * 10) / 10.0);
   if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     if(!noDhtReported) {
-      Blynk.logEvent("no_dht_sensor");
+      Blynk.logEvent("no_dht_sensor"+String("MAC:")+WiFi.macAddress());
       noDhtReported = true;
     }
     

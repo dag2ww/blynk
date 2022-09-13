611f3e179a09cb3e02cab59623ab658900e4a596
Grzegorz <dag2ww@gmail.com>
2022-09-13 19:22:32 +0200

    cleanup

--- blynk_esp_dht22_bmp180_wemosD1mini_OTA_Blynk_Inject/blynk_esp_dht22_bmp180_wemosD1mini_OTA_Blynk_Inject.ino
+++ blynk_esp_dht22_bmp180_wemosD1mini_OTA_Blynk_Inject/blynk_esp_dht22_bmp180_wemosD1mini_OTA_Blynk_Inject.ino
@@ -1,6 +1,6 @@
 #define BLYNK_TEMPLATE_ID "TMPLyiiIu_zF"
 #define BLYNK_DEVICE_NAME "Klimat Info"
-#define BLYNK_FIRMWARE_VERSION        "0.3.5"
+#define BLYNK_FIRMWARE_VERSION        "0.3.6"
 #define BLYNK_PRINT Serial1
 //#define BLYNK_DEBUG
 #define APP_DEBUG
@@ -52,6 +52,8 @@ void setup() {
   delay(3000);
   // put your setup code here, to run once:
   Serial.begin(115200);
+  Serial1.begin(115200);
+  
   Serial.println();
   Serial.println("Starting...");    //read configuration from FS json
 
@@ -60,12 +62,10 @@ void setup() {
 
   parser.registerCommand("|CMD|--CWU_RUN_STATE", "i", &cmd_cwu_on_feedback_process);
   parser.registerCommand("|CMD|--CWU_TEMPERATURE", "d", &cmd_cwu_temp_process);
-    
   
   BlynkEdgent.begin();
+  Blynk.logEvent("started");
   
-  Blynk.logEvent("started_info");
-  //Blynk.logEvent("started_info", String("MAC:")+WiFi.macAddress());
   dht.begin();
 
   //SDA, SCL
@@ -78,12 +78,12 @@ void setup() {
     bmpPresent = true;
   }
 
-  blynkTimer.setInterval(2500L, blynkPush);
+  blynkTimer.setInterval(1500L, blynkPush);
 }
 
 //// This function will run every time Blynk connection is established
 void BlynkOnConnected() {
-    ticksToRestart = 60;
+    ticksToRestart = 120;
     // Request Blynk server to re-send latest values for all pins
     Blynk.syncAll();
 }
@@ -99,16 +99,14 @@ void blynkPush()
   }
   
   float h = dht.readHumidity();
-  // Read temperature as Celsius
   float t = dht.readTemperature();
   
   if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
+    Serial1.println("Failed to read from DHT sensor!");
     if(!noDhtReported) {
       Blynk.logEvent("no_dht_sensor");
       noDhtReported = true;
     }
-    
     dhtReadErrorCount += 1;
     if(dhtReadErrorCount >= 10) {
       Blynk.virtualWrite(V1, -127);
@@ -118,15 +116,14 @@ void blynkPush()
   } else {
     Blynk.virtualWrite(V1, t);
     Blynk.virtualWrite(V2, h); 
-    double dp = dewPoint(t,h);
-    Blynk.virtualWrite(V4, dp); 
+    Blynk.virtualWrite(V4, dewPoint(t,h)); 
     dhtReadErrorCount = 0;  
-    //Serial.println("Temp: "+String(t)+" Humidity: "+String(h)+".");
+    Serial1.println("Temp: "+String(t)+" Humidity: "+String(h)+".");
   }
 
   if(bmpPresent) {
     float p = bmp.readPressure()/100;
-    //Serial.println("Pressure: " + String(p));
+    Serial1.println("Pressure: " + String(p));
     Blynk.virtualWrite(V3, p);
   } 
 
@@ -146,11 +143,9 @@ double dewPoint(double celsius, double humidity)
   RHS += -1.3816e-7 * (pow(10, (11.344 * (1 - 1/RATIO ))) - 1) ;
   RHS += 8.1328e-3 * (pow(10, (-3.49149 * (RATIO - 1))) - 1) ;
   RHS += log10(1013.246);
-
-        // factor -3 is to adjust units - Vapor Pressure SVP * humidity
+  // factor -3 is to adjust units - Vapor Pressure SVP * humidity
   double VP = pow(10, RHS - 3) * humidity;
-
-        // (2) DEWPOINT = F(Vapor Pressure)
+  // (2) DEWPOINT = F(Vapor Pressure)
   double T = log(VP/0.61078);   // temp var
   return (241.88 * T) / (17.558 - T);
 }
@@ -170,22 +165,22 @@ void loop()
     }
     if(line[0] == '|'){
       char response[MyCommandParser::MAX_RESPONSE_SIZE];
-      //Serial.println(String("Got and will process:")+line);
+      Serial1.println(String("Got and will process:")+line);
       parser.processCommand(line, response);
-      //Serial.println(response);
+      Serial1.println(response);
     }
   }
   
 }
 
 void cmd_cwu_on_feedback_process(MyCommandParser::Argument *args, char *response) {
-  //Serial.println(String("setting pracaCWUFeedback to: ")+args[0].asInt64);
+  Serial1.println(String("setting pracaCWUFeedback to: ")+args[0].asInt64);
   pracaCWUFeedback = args[0].asInt64;
   strlcpy(response, "success", MyCommandParser::MAX_RESPONSE_SIZE);
 }
 
 void cmd_cwu_temp_process(MyCommandParser::Argument *args, char *response) {
-  //Serial.println(String("setting temperaturaCWU to: ")+args[0].asDouble);
+  Serial1.println(String("setting temperaturaCWU to: ")+args[0].asDouble);
   temperaturaCWU = args[0].asDouble;
   strlcpy(response, "success", MyCommandParser::MAX_RESPONSE_SIZE);
 }
